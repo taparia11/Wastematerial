ab5c267384801f2fb05b11c82136f6cbf3716344
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-06-15 10:23:12 +0530

    adding data

---
+++ src/Components/Addnote.js
@@ -0,0 +1,39 @@
+import React, { useContext, useState } from "react";
+import noteContext from "../context/notes/noteContext";
+
+const Addnote = (props) => {
+  const context = useContext(noteContext);
+  const { addNote } = context;
+  const [note, setNote] = useState({title:"", description:"",tag:""})
+  const handleClick = (e)=>{
+      e.preventDefault();
+    addNote(note.title, note.description, note.tag);
+    props.showAlert("Note created successfully","success")
+    setNote({title:"", description:"",tag:""})
+  }
+  const onChange = (e)=>{
+    setNote({...note, [e.target.name]: e.target.value })
+}
+
+  return (
+      <div className='container'>
+          <form>
+              <div className="mb-3">
+                  <label htmlFor="title" className="form-label"> Title </label>
+                  <input type="email" className="form-control" id="title" name="title" aria-describedby="emailHelp" value={note.title} onChange={onChange} minLength={5} required />
+              </div>
+              <div className="mb-3">
+                  <label htmlFor="description" className="form-label"> Description </label>
+                  <input type="text" className="form-control" id="description" name="description" value={note.description} onChange={onChange} minLength={5} required />
+              </div>
+              <div className="mb-3">
+                  <label htmlFor="tag" className="form-label"> Tag </label>
+                  <input type="text" className="form-control" id="tag" name="tag" value={note.tag} onChange={onChange} minLength={5} required />
+              </div>
+              <button disabled={note.title.length<5 || note.description.length<5} type="submit" className="btn btn-primary" onClick={handleClick}> Add Note </button>
+          </form>
+      </div>
+  );
+};
+
+export default Addnote;
