848254ee354730521fe072ecbff6332ebcc37272
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-06-16 04:02:53 +0530

    logo and name updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = (props) => {
   return ( 
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">{props.title}</Link>
+    <Link className="navbar-brand" to="/"><img src={require('./guvi.png')} alt="" width="140" height="64"/></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
