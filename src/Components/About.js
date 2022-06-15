5ec29c9bd79f19997eca3aabf0c094116939e50f
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-06-15 23:45:37 +0530

    content added

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,10 +1,21 @@
 import React,{useContext} from 'react'
 import NoteContext from '../context/notes/noteContext'
+import { Container, Card, Row, Text } from "@nextui-org/react";
 
 function About() {
   const a = useContext(NoteContext)
   return (
-    <div>About {a.name}</div>
+    <Container xl>
+      <Card css={{ $$cardColor: '$colors$primary' }}>
+        <Card.Body>
+          <Row justify="center" align="center">
+            <Text h6 size={15} color="white" css={{ m: 0 }}>
+             GCC always known for his work towards community and socity.
+            </Text>
+          </Row>
+        </Card.Body>
+      </Card>
+    </Container>
   )
 }
 
